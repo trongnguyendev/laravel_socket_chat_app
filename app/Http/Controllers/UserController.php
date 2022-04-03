a156e188166bb01549563ce25b5dec45f1a4db48
Trong Dev <trongnguyeendev@gmail.com>
2022-04-03 17:33:44 +0700

    ..

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Events\NewMessage;
+
+class UserController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user','token'), 201);
+    }
+
+    public function me()
+    {
+        broadcast(new App\Events\NewMessage())->toOthers();
+        return response()->json("okkkk");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route::get('/getUser', function() {
+// 	return 'ok';
+// });
+
+// Route::get('/messages', function() {
+//     $message = 'message ne';
+//     $user = 'TN';
+//    broadcast(new NewMessage($message, $user));
+//    return 'ok';
+//  });
+
+
+Route::post('register', [UserController::class, 'register']);
+
+Route::group([
+    'middleware' => 'api',
+    'prefix'     => 'auth'
+], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    // Route::post('me', [AuthController::class, 'me']);
+});
+
+Route::get('me', [UserController::class, 'me']);
